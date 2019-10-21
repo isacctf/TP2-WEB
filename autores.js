0f76f35b87a89c28a0343d6570b16e312fe0e519
Your Name <you@example.com>
2019-10-21 13:36:13 -0300

    commit inicial

---
+++ autores.js
@@ -0,0 +1,15 @@
+let janelamodal = document.querySelector("#janelamodal");
+let criadoras = document.querySelector("#criadoras");
+let span = document.querySelectorAll('.close')[0];
+criadoras.addEventListener("click", function(e) {
+  e.preventDefault();
+  janelamodal.style.display = "block";
+});
+span.addEventListener("click", function(e){
+  janelamodal.style.display = "none";
+});
+window.addEventListener("click", function(e) {
+  if (event.target == janelamodal) {
+    janelamodal.style.display = "none";
+  }
+});
